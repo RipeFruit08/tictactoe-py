2ca14c20b057662b262f97b5c413f03716836568
RipeFruit08 <syk4367@rit.edu>
2020-11-01 10:31:52 -0500

    Started cleaining code, added documentation

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,4 @@
+# set BOARD_DIM value to determine size of tic tac toe board (3 is default)
 BOARD_DIM = 3
 BOARD = [
     ['-']*BOARD_DIM for _ in range(BOARD_DIM)
@@ -26,28 +27,45 @@ def process_cmd(cmd, player_one):
         print("Quit signal received, exiting...")
         exit()
     else:
-        cmd_split = cmd.split()
-        board_dim = len(BOARD)
-        row = int(cmd_split[0])
-        col = int(cmd_split[1])
-        if row >= board_dim or col >= board_dim:
-            print("This was not a valid move, try again")
-            return False
-        val = "X" if player_one else "O"
-        if BOARD[row][col] == "X" or BOARD[row][col] == "O":
-            print("This was not a valid move, try again")
-            return False
-        else:
+        # processes command to place piece. this command should be in the form
+        # of two numbers separated by spaces For example the command "0 0" would 
+        # place the player's piece on the board at (0,0)
+        res = sanitize_input(cmd)
+        if res:
+            row, col = res[0], res[1]
             BOARD[row][col] = val
             return True
+        else:
+            # should be returning false 
+            return res
+
+"""
+validates tic tac toe input 
+
+:param cmd: the raw command from tic tac toe game 
+:returns: tuple containing row and column coordinates if input is valid
+          otherwise returns False
+:raises ValueError: # todo handle this exception among any others 
+"""
+def sanitize_input(cmd):
+    cmd_split = cmd.split()
+    row = int(cmd_split[0])
+    col = int(cmd_split[1])
+    if row >= BOARD_DIM or col >= BOARD_DIM:
+        print("This was not a valid move, try again")
+        return False
+    val = "X" if player_one else "O"
+    if BOARD[row][col] == "X" or BOARD[row][col] == "O":
+        print("This was not a valid move, try again")
+        return False
+    return (row, col)
 
 def determine_winner():
-    board_dim = len(BOARD)
     # check rows
-    for row in range(board_dim):
+    for row in range(BOARD_DIM):
         x_count = 0
         o_count = 0
-        for col in range(board_dim):
+        for col in range(BOARD_DIM):
             val = BOARD[row][col]
             if val == "X":
                 x_count += 1
@@ -55,10 +73,10 @@ def determine_winner():
                 o_count += 1
         win_cond(x_count, o_count)
     # check columns
-    for col in range(board_dim):
+    for col in range(BOARD_DIM):
         x_count = 0
         o_count = 0
-        for row in range(board_dim):
+        for row in range(BOARD_DIM):
             val = BOARD[row][col]
             if val == "X":
                 x_count += 1
@@ -72,7 +90,7 @@ def determine_winner():
     # first diagonal (back slash)
     x_count = 0
     o_count = 0
-    for coord in range(board_dim):
+    for coord in range(BOARD_DIM):
         val = BOARD[coord][coord]
         if val == "X":
             x_count += 1
@@ -83,8 +101,8 @@ def determine_winner():
     # second diagonal (forward slash)
     x_count = 0
     o_count = 0
-    for coord in range(board_dim):
-        val = BOARD[(board_dim-1)-coord][coord]
+    for coord in range(BOARD_DIM):
+        val = BOARD[(BOARD_DIM-1)-coord][coord]
         if val == "X":
             x_count += 1
         elif val == "O":
